005c6d05ae4139c5472b7395122bb0b028891533
Evandro Roson dos Santos <evandroroson@yahoo.com.br>
2023-07-25 17:04:50 -0300

    insert styled-components

--- AppVendasOnline/index.js
+++ AppVendasOnline/index.js
@@ -1,13 +1,9 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
  * @format
  */
 
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import App from './src/App';
-
 
 AppRegistry.registerComponent(appName, () => App);
